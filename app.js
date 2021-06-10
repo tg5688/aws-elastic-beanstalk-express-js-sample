3bc7c38e60dd6dcfe8ef9366d5f8225fb681389b
Tomasz Gunter <tgunter@future-processing.com>
2021-06-10 08:38:26 +0200

    Pipeline test 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Po prostu zmiana'));
+app.get('/', (req, res) => res.send('Po prostu kolejna zmiana'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
